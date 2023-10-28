f00f082338ce3a445fb3314ae87e50c03831e780
King-George-Hughes <hughesgeorgeking@gmail.com>
2023-10-28 15:26:56 +0000

    changes

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
